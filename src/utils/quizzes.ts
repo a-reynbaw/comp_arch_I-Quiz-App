70d734591518c3c99993aba8d8b16e6d5ef9f75a
ANASTASIA-MARIA MARINAKOU <sdi2400120@linux17.di.uoa.gr>
2025-05-30 18:45:36 +0300

    added rest of the quizzes

--- src/utils/quizzes.ts
+++ src/utils/quizzes.ts
@@ -1,5 +1,14 @@
 import quiz1 from '../data/quiz1.json';
 import quiz2 from '../data/quiz2.json';
+import quiz3 from '../data/quiz3.json';
+import quiz4 from '../data/quiz4.json';
+import quiz5 from '../data/quiz5.json';
+import quiz6 from '../data/quiz6.json';
+import quiz7 from '../data/quiz7.json';
+import quiz8 from '../data/quiz8.json';
+import quiz9 from '../data/quiz9.json';
+import quiz10 from '../data/quiz10.json';
+import quiz11 from '../data/quiz11.json';
 
 export const quizzes = [
   {
@@ -14,4 +23,58 @@ export const quizzes = [
     description: '',
     questions: quiz2,
   },
+  {
+    id: 'quiz-3',
+    title: 'Κουίζ 3',
+    description: '',
+    questions: quiz3,
+  },
+  {
+    id: 'quiz-4',
+    title: 'Κουίζ 4',
+    description: '',
+    questions: quiz4,
+  },
+  {
+    id: 'quiz-5',
+    title: 'Κουίζ 5',
+    description: '',
+    questions: quiz5,
+  },
+  {
+    id: 'quiz-6',
+    title: 'Κουίζ 6',
+    description: '',
+    questions: quiz6,
+  },
+  {
+    id: 'quiz-7',
+    title: 'Κουίζ 7',
+    description: '',
+    questions: quiz7,
+  },
+  {
+    id: 'quiz-8',
+    title: 'Κουίζ 8',
+    description: '',
+    questions: quiz8,
+  },
+  {
+    id: 'quiz-9',
+    title: 'Κουίζ 9',
+    description: '',
+    questions: quiz9,
+  },
+  {
+    id: 'quiz-10',
+    title: 'Κουίζ 10',
+    description: '',
+    questions: quiz10,
+  },
+  {
+    id: 'quiz-11',
+    title: 'Κουίζ 11',
+    description: '',
+    questions: quiz11,
+  }
 ];
