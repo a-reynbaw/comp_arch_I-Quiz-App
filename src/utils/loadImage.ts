fb7cd7d282795c89ad346d92ab94b0e07e70799a
ANASTASIA-MARIA MARINAKOU <sdi2400120@linux17.di.uoa.gr>
2025-05-30 21:33:51 +0300

    .

--- src/utils/loadImage.ts
+++ src/utils/loadImage.ts
@@ -1,6 +1,6 @@
 export const loadImage = async (imageKey: string) => {
   try {
-    const image = await import(`../assets/${imageKey}.jpg`);
+    const image = await import(`../assets/${imageKey}.png`);
     return image.default;
   } catch (error) {
     console.error(`Image not found: ${imageKey}`, error);
